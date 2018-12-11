f63c17ae81243226d4245a2e0243f9f203f06556
Kat Sullivan <kmsullivan012@gmail.com>
2018-12-11 15:19:47 -0500

    added contact info

---
+++ app.js
@@ -0,0 +1,30 @@
+$(documnet).ready(function(){
+
+  $('#myButton').click(function() {
+    var name = $('.first').val() + " " + $('.last');
+    var email = $('.email').val();
+    var message = $('textarea').val();
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: {
+        'key': '0t-klM9r1Do4mNPmQBs3Cw',
+        'message': {
+          'from_email': 'kmsullivan012@gmail.com',
+          'to': [
+              {
+                'email': email,
+                'name': name,
+                'type': 'to'
+              }
+            ],
+          'autotext': 'true',
+          'subject': 'Contact Form',
+          'html': message
+        }
+      }
+    });
+  });
+});
+
+
